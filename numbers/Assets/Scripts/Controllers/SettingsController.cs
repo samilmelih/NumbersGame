fc641115f9efe837c244843bf2db7af6dd585bd8
SOFTSAM <sam96mel95@gmail.com>
2017-12-20 08:50:43 +0100

    Audio mixer added

--- numbers/Assets/Scripts/Controllers/SettingsController.cs
+++ numbers/Assets/Scripts/Controllers/SettingsController.cs
@@ -1,20 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
+    public AudioMixer audioMixer;
+    public Slider slider;
+    float mutedValue = -80f;
+    float unMutedValue = -60f;
+    // Use this for initialization
+    private void Start()
+    {
+        float val;
+        audioMixer.GetFloat("volume",out val);
+
+        slider.value = val;
+    }
+    public void SetVolume(float value)
+    {
+        audioMixer.SetFloat("volume", value);
+    }
+
+    public void MuteVolume(bool mute)
+    {
+        if (mute)
+        {
+            audioMixer.SetFloat("volume", mutedValue);
+            slider.value = mutedValue;
+        }
+        else
+        {
+            audioMixer.SetFloat("volume", unMutedValue);
+            slider.value = unMutedValue;
+        }
+    }
 	public void ResetProgress()
 	{
 		ProgressController.ResetProgress();
