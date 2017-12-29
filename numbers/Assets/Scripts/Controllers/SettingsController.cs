b356425c00311ef85f97de88a35d8e20f90c854c
SOFTSAM <sam96mel95@gmail.com>
2017-12-29 14:54:00 +0100

    Settings and how to play changed

--- numbers/Assets/Scripts/Controllers/SettingsController.cs
+++ numbers/Assets/Scripts/Controllers/SettingsController.cs
@@ -7,19 +7,44 @@ using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour
 {
+    public Button btnSliderOn;
+    public Button btnSliderOff;
     public Slider slider;
 	const float mutedVolume = 0f;
 	const float defaultVolume = 1f;
+
 
     // Use this for initialization
     private void Start()
     {
 		slider.value = DataTransfer.volume;
+        
     }
-
+    float oldVal=0;
     public void SetVolume(float value)
     {
 		DataTransfer.volume = value;
+       
+        if(value == slider.minValue)
+        {
+            btnSliderOn.gameObject.SetActive(false);
+            btnSliderOff.gameObject.SetActive(true);
+        }
+        else
+        {
+            btnSliderOn.gameObject.SetActive(true);
+        }
+        if (oldVal == 0 && value > 0)
+        {
+            btnSliderOff.gameObject.SetActive(false);
+        }
+        //else
+        //{
+        //   
+        //}
+        oldVal = value;
+
+
     }
 
     public void MuteVolume(bool mute)
